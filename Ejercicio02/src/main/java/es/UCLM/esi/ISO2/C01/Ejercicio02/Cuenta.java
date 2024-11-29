f3d7164467c0de6e3d0d168265ab81c1c37b24e2
Samuel Rodero Ballesteros <samuel.rodero@alu.uclm.es>
2024-11-29 10:04:03 +0100

    Fin de la clase cuenta

--- Ejercicio02/src/main/java/es/UCLM/esi/ISO2/C01/Ejercicio02/Cuenta.java
+++ Ejercicio02/src/main/java/es/UCLM/esi/ISO2/C01/Ejercicio02/Cuenta.java
@@ -1,39 +1,62 @@
 package es.UCLM.esi.ISO2.C01.Ejercicio02;
 
-/**
- * Hello world!
- */
 public class Cuenta {
     public String determinar_cuenta(Cliente c) {
-
+    	
     	String cuenta = "";
-    	
-    	if(c.getEdad()<18 && c.isEstudia()==true && c.isVivePadres()==true) {
-    		cuenta = "Cuenta confort";
+    	/*Primero controlamos si traba para disminuir las combinaciones de los siguientes if. Porque como se puede dar el caso
+    	 * donde trbaje y estudie, priorizamos que trabaje a que estudie. Por lo que los casos de que sea estudiante se darán
+    	 * si y solo si estudia, mientras que los de trabaja se darán si trabaja o si trabja y estudia que es más lógico.
+    	 * */
+    	if(c.isTrabaja()==true) {
+    		
+    		//Si es mayor de 25 y va implicito que trabaja y no estudia puede:
+    		if(c.getEdad()>25) {
+    			
+    			//Vivir con sus padres
+    			if(c.isVivePadres()==true) {
+    				//Caso 4
+    				cuenta = "Cuenta Independízate que va siendo hora";
+    			}
+    			//No vive con sus padres
+    			else {
+    				//Caso 5
+    				cuenta = "Cuenta Bienvenido a la Vida Adulta";
+    			}
+    			
+    		}
+    		//Si es mayor de 18 pero menor de 25, y va implicito que trabaja, puede:
+    		else if(c.getEdad()>18) {
+    			
+    			//Vivir con sus padres
+    			if(c.isVivePadres()==true) {
+    				//Caso 3.1
+    				cuenta = "Cuenta Ahorra ahora que puedes";
+    			}
+    			//No vivir con sus padres
+    			else {
+    				//Caso 3.2
+    				cuenta = "Cuenta Saltando del Nido";
+    			}
+    		}
     	}
     	
-    	else if(c.getEdad()<25 && c.isEstudia()==true && c.isVivePadres()==false) {
-    		cuenta = "Cuenta Vamos que tu puedes";
+    	//Si no trabaja y si estudia
+    	else if(c.isEstudia()==true) {
+    		
+    		
+    		//En estos dos ifs ya va implicito que está estudiando, por lo que controlamos las demás condiciones
+    		if(c.getEdad()<18 && c.isVivePadres()==true) {
+    			//Caso 1
+        		cuenta = "Cuenta confort";
+        	}
+    		
+    		else if(c.getEdad()<25 &&  c.isVivePadres()==false) {
+    			//Caso 2
+        		cuenta = "Cuenta Vamos que tu puedes";
+        	}
     	}
-    	
-    	else if(c.getEdad()>18 && c.isTrabaja()==true && c.isVivePadres()==true) {
-    		cuenta = "Cuenta ahorra ahora que pudes";
-    	}
-    	
-    	else {
-    		cuenta = "Saltando del nido";
-    	}
-    	
-    	if(c.getEdad()>25 && c.isTrabaja()==true && c.isVivePadres()==true) {
-    		cuenta = "Cuenta independizate que va siendo hora";
-    	}
-    	
-    	else if(c.getEdad()>25 && c.isTrabaja()==true && c.isVivePadres()==false){
-    		cuenta = "Cuenta bienvenido a la vida adulta";
-    	}
-    	
-    	
-    	
+    	    	
     	return cuenta;
         
     }
