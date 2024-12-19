ac22b13ee62f4be98b8c780a576d6ad4d6da3822
KevinRuedas183 <Kevin.Ruedas@alu.uclm.es>
2024-12-19 10:16:14 +0100

    Implementación de las pruebas del problema 2 con JUnit terminada.

--- Ejercicio02/src/test/java/es/UCLM/esi/ISO2/C01/Ejercicio02/CuentaTest.java
+++ Ejercicio02/src/test/java/es/UCLM/esi/ISO2/C01/Ejercicio02/CuentaTest.java
@@ -1,4 +1,4 @@
-package es.UCLM.esi.ISO2.C01.ejercicio02;
+package es.UCLM.esi.ISO2.C01.Ejercicio02;
 
 import static org.junit.Assert.*;
 
@@ -10,7 +10,8 @@ public class CuentaTest {
 	public void testTrabajaMayor25Padres() {
 		try {
 			Cliente c = new Cliente(26, true, false, true);
-			String res = Cuenta.determinar_cuenta(c);
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
 			String expected = "Cuenta Independízate que va siendo hora";
 			assertEquals(expected, res);
 		} catch (Exception e) {
@@ -18,11 +19,24 @@ public class CuentaTest {
 	}
 	
 	@Test
-	public void testNoTrabajaMenor25NoPadres() {
+	public void testTrabajaMayor25NoPadres() {
 		try {
-			Cliente c = new Cliente(24, false, false, false);
-			String res = Cuenta.determinar_cuenta(c);
-			String expected = "Cuenta Vamos que tu puedes";
+			Cliente c = new Cliente(26, true, false, false);
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
+			String expected = "Cuenta Bienvenido a la Vida Adulta";
+			assertEquals(expected, res);
+		} catch (Exception e) {
+		}
+	}
+	
+	@Test
+	public void testTrabajaMayor18Menor25NoPadres() {
+		try {
+			Cliente c = new Cliente(24, true, false, false);
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
+			String expected = "Cuenta Saltando del Nido";
 			assertEquals(expected, res);
 		} catch (Exception e) {
 		}
@@ -32,7 +46,8 @@ public class CuentaTest {
 	public void testTrabajaMayor18Menor25Padres() {
 		try {
 			Cliente c = new Cliente(19, true, false, true);
-			String res = Cuenta.determinar_cuenta(c);
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
 			String expected = "Cuenta Ahorra ahora que puedes";
 			assertEquals(expected, res);
 		} catch (Exception e) {
@@ -43,8 +58,9 @@ public class CuentaTest {
 	public void testTrabajaMenor25Padres() {
 		try {
 			Cliente c = new Cliente(18, false, true, true);
-			String res = Cuenta.determinar_cuenta(c);
-			String expected = "Cuenta Vamos que tu puedes";
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
+			String expected = "";
 			assertEquals(expected, res);
 		} catch (Exception e) {
 		}
@@ -54,7 +70,8 @@ public class CuentaTest {
 	public void testTrabajaMenor18Padres() {
 		try {
 			Cliente c = new Cliente(17, false, true, true);
-			String res = Cuenta.determinar_cuenta(c);
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
 			String expected = "Cuenta confort";
 			assertEquals(expected, res);
 		} catch (Exception e) {
@@ -65,29 +82,21 @@ public class CuentaTest {
 	public void testTrabajaMayor18NoPadres() {
 		try {
 			Cliente c = new Cliente(17, false, false, false);
-			String res = Cuenta.determinar_cuenta(c);
-			assertNull(res);
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
+			assertEquals("", res);
 		} catch (Exception e) {
 		}
 	}
 	
 	@Test
-	public void testTrabajaMenor25Padres() {
-		try {
-			Cliente c = new Cliente(19, false, true, true);
-			String res = Cuenta.determinar_cuenta(c);
-			String expected = "Cuenta Vamos que tu puedes";
-			assertEquals(expected, res);
-		} catch (Exception e) {
-		}
-	}
-	
-	@Test
-	public void testTrabajaMenor25NoPadres() {
+	public void testNoTrabajaMenor25NoPadres() {
 		try {
 			Cliente c = new Cliente(17, false, true, false);
-			String res = Cuenta.determinar_cuenta(c);
-			assertNull(res);
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
+			String expected = "Cuenta Vamos que tu puedes";
+			assertEquals(expected, res);
 		} catch (Exception e) {
 		}
 	}
@@ -96,8 +105,11 @@ public class CuentaTest {
 	public void testEdadNegativa() {
 		try {
 			Cliente c = new Cliente(-1, false, true, false);
-			assertNotNull(res);
+			Cuenta cuenta = new Cuenta();
+			String res = cuenta.determinar_cuenta(c);
+			assertEquals("", res);
 		} catch (Exception e) {
+			assertTrue(true);
 		}
 	}
 	
